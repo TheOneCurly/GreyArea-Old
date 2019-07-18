aea61a145aac6e665e6a3bbda0845636361a2321
Alex Wilson <rugbylg6#gmail.com>
2019-07-18 11:01:17 -0400

    Constant strings were not being set properly, changed to literals temporarily to fix the seg fault. #2

--- cpp/src/ds18b20.cpp
+++ cpp/src/ds18b20.cpp
@@ -1,21 +1,22 @@
 #include "ds18b20.h"
 #define debug std::cout << "DS18B20: "
 
-const std::string DS18B20::DevicesPath = "/sys/bus/w1/devices";
-const std::string DS18B20::SensorPartialPath = "28-";
-const std::string DS18B20::ReadPath = "w1_slave";
+const std::string DS18B20::DevicesPath("/sys/bus/w1/devices");
+const std::string DS18B20::SensorPartialPath("28-");
+const std::string DS18B20::ReadPath("w1_slave");
 
 std::string DS18B20::discover(){
     std::string sensorPath = "";
-
-    DIR* devicesDir = opendir(DevicesPath.c_str());
+//    DIR* devicesDir = opendir(DevicesPath.c_str());
+    DIR* devicesDir = opendir("/sys/bus/w1/devices");
     if(devicesDir != NULL){
         dirent* sensorDir = readdir(devicesDir);
         std::string item;
 
         while(sensorDir != NULL){
             item = sensorDir->d_name;
-            size_t found = item.find(SensorPartialPath);
+//            size_t found = item.find(SensorPartialPath);
+            size_t found = item.find("28-");
 
             if(found != std::string::npos){
                 sensorPath = item;
@@ -27,7 +28,7 @@ std::string DS18B20::discover(){
         closedir(devicesDir);
     }else{
         int errsv = errno;
-        debug << "error opening sensor directory " << errsv;
+        debug << "error opening sensor directory " << errsv << std::endl;
     }
 
     return sensorPath;
@@ -37,7 +38,8 @@ float DS18B20::read(std::string sensor){
     float temp = 0;
 
     std::ifstream tempStream;
-    tempStream.open((DevicesPath + "/" + sensor + "/" + ReadPath).c_str());
+//    tempStream.open((DevicesPath + "/" + sensor + "/" + ReadPath).c_str());
+    tempStream.open((DevicesPath + "/" + sensor + "/w1_slave").c_str());
 
     std::string statusLine;
     std::getline(tempStream, statusLine);

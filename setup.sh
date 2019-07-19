cc9c68c212e88414194ceca5223137b93a54a60d
Alex Wilson <rugbylg6#gmail.com>
2019-07-19 15:43:29 -0400

    Added checks to setup script.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-# Detect System
+# Check for sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 # Set Kernel Modules
 ln -s /home/pi/config/etc/modules /etc/modules
 
+# Install Python
+dpkg -s python3 &> /dev/null
+retVal = $?
+if [ $? -eq 1 ]
+then
+    apt -y install python3
+fi
+
 # Install Nginx
-apt -y install nginx
+dpkg -s nginx &> /dev/null
+retVal = $?
+if [ $? -eq 1 ]
+then
+    apt -y install nginx
+fi
 
 # Link nginx configuration
 ln -s /home/pi/config/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 ln -s /home/pi/config/etc/nginx/sites-available/greyarea /etc/nginx/sites-available/greyarea
 
 # Install Supervisor
-apt -y install supervisor
+dpkg -s supervisor &> /dev/null
+retVal = $?
+if [ $? -eq 1 ]
+then
+    apt -y install supervisor
+fi
 
 # Link supervisor configuration
 ln -s /home/pi/config/etc/supervisor/conf.d/greyarea.conf /etc/supervisor/conf.d/greyarea.conf
